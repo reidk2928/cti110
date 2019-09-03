1c1a76f03b5345adab31454a1ffdb31f9667048d
reidk2928 <54859120+reidk2928@users.noreply.github.com>
2019-09-03 13:30:04 -0400

    First submission

---
+++ P1T1_ReidKareem (1).py
@@ -0,0 +1,10 @@
+>>> print('This is a test of IDLE.')
+This is a test of IDLE.
+>>> print(This is a test of IDLE.)
+SyntaxError: invalid syntax
+>>> print('This is a test of IDLE.'
+
+
+      )
+This is a test of IDLE.
+>>> 
